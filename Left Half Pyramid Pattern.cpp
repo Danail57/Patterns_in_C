8160ccbfb7e59a7bd83522f474b24665b27fa9e9
Danail Donkov <danail.donkov5@gmail.com>
2026-01-21 14:25:48 +0200

    Create Left Half Pyramid Pattern.cpp

---
+++ Left Half Pyramid Pattern.cpp
@@ -0,0 +1,22 @@
+//Left Half Pyramid Pattern
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int rows;
+	cin >> rows;
+
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < 2 * (rows - i) - 1; j++)
+		{
+			cout << " ";
+		}
+		for (int k = 0; k <= i; k++)
+		{
+			cout << "* ";
+		}cout << endl;
+	}
+	return 0;
+}
