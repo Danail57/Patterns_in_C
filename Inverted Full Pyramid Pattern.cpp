22c289ccd8923ba054e5d9c46061eb569162547d
Danail Donkov <danail.donkov5@gmail.com>
2026-01-21 14:40:34 +0200

    Create Inverted Full Pyramid Pattern.cpp

---
+++ Inverted Full Pyramid Pattern.cpp
@@ -0,0 +1,23 @@
+// Inverted Full Pyramid Pattern
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int rows;
+	cin >> rows;
+
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			cout << " ";
+		}
+
+		for (int k = 0; k < 2 * (rows - i) - 1; k++)
+		{
+			cout << "*";
+		}
+		cout << endl;
+	}
+}
